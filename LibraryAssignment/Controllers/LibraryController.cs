7b9c510e8c087d594007eb928aa400a6e6b88ab8
Raihan Saputra <vanquish00vip@gmail.com>
2024-08-03 21:23:47 +0700

    Add project files.

---
+++ LibraryAssignment/Controllers/LibraryController.cs
@@ -0,0 +1,42 @@
+﻿using LibraryAssignment.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAssignment.Controllers
+{
+    [Route("api/library")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private static List<Book> _bookCollection = new List<Book>();
+
+        [HttpPost("add")]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            if (!_bookCollection.Any(b => b.Title == book.Title))
+            {
+                _bookCollection.Add(book);
+                return Ok(new { message = "Buku berhasil ditambah" });
+            }
+            return BadRequest(new { message = "Buku sudah ada" });
+        }
+
+        [HttpDelete("remove/{title}")]
+        public IActionResult RemoveBook(string title)
+        {
+            var book = _bookCollection.FirstOrDefault(b => b.Title == title);
+            if (book != null)
+            {
+                _bookCollection.Remove(book);
+                return Ok(new { message = "Buku berhasil dihapus" });
+            }
+            return NotFound(new { message = "Buku tidka ada" });
+        }
+
+        [HttpGet("display")]
+        public ActionResult<List<Book>> DisplayBooks()
+        {
+            return Ok(_bookCollection);
+        }
+    }
+}
---
+++ LibraryAssignment/Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAssignment.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
